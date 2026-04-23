f53a7165d51be52246799904ddf517e4498acf5e
psbella <pablo.s.bella@gmail.com>
2026-04-23 20:26:49 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'farmaturnos-v1';
+const urls = [
+  '/turnos/',
+  '/turnos/index.html',
+  '/turnos/manifest.json',
+  '/turnos/ciclos_final.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urls)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
