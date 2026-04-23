9d251e8f4ecf1a0f120fa3fec5275cc949f141a8
psbella <pablo.s.bella@gmail.com>
2026-04-23 01:45:43 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'farmacias-mdp-v1';
+const urlsToCache = [
+  '/turnos/',
+  '/turnos/index.html',
+  '/turnos/manifest.json',
+  '/turnos/ciclos_final.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
