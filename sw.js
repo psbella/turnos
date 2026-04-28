a30bcac1f9c907546929a5ee4c11a2355c04bc14
psbella <pablo.s.bella@gmail.com>
2026-04-27 21:02:28 -0300

    Replace ciclos_final_definitivo.json with ciclos_oficiales_definitivo.json

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const urls = [
   '/turnos/',
   '/turnos/index.html',
   '/turnos/manifest.json',
-  '/turnos/ciclos_final_definitivo.json'
+  '/turnos/ciclos_oficiales_definitivo.json'
 ];
 
 self.addEventListener('install', e => {
