bcc8420a339dd3c9bbca6320bd17a59873b813ad
Pablo Bella <pablo.s.bella@gmail.com>
2026-05-04 14:02:32 -0300

    Refactor limpiarMarcadores and update exports

--- js/maps.js
+++ js/maps.js
@@ -29,8 +29,8 @@ export function initMaps() {
 }
 
 export function limpiarMarcadores() {
-  markersDesktop.forEach(m => mapDesktop.removeLayer(m));
-  markersMobile.forEach(m => mapMobile.removeLayer(m));
+  if (mapDesktop) markersDesktop.forEach(m => mapDesktop.removeLayer(m));
+  if (mapMobile) markersMobile.forEach(m => mapMobile.removeLayer(m));
   markersDesktop = [];
   markersMobile = [];
 }
@@ -44,7 +44,7 @@ export function agregarMarcadores(farmacias) {
     const c = f.lat && f.lng ? [f.lat, f.lng] : null;
     farmaciasCoords[i] = c;
 
-    const pop = `<b>${f.nombre}</b><br>${f.direccion}<br>${getPhoneLink(f)}<br><a href="${getGoogleMapsLink(f.direccion)}" target="_blank" class="gmaps-link">Google Maps</a>`;
+    const pop = `<b>${f.nombre}</b><br>${f.direccion}<br>${getPhoneLink(f)}<br><a href="${getGoogleMapsLink(f.direccion)}" target="_blank" class="gmaps-link" rel="noopener noreferrer">Google Maps</a>`;
 
     if (c) {
       const mD = L.marker(c, { icon: pharmacyIcon }).addTo(mapDesktop).bindPopup(pop);
@@ -95,4 +95,4 @@ function getPhoneIcon() {
   return `<span class="icon-phone"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" fill="var(--accent)"/></svg></span>`;
 }
 
-export { markersDesktop, markersMobile };
+export { mapDesktop, mapMobile, markersDesktop, markersMobile };
