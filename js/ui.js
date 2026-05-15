a68c8e1bbd9dced7a9e3d360a29eccdf33f3b396
Pablo Bella <pablo.s.bella@gmail.com>
2026-05-15 14:29:07 -0300

    Refactor mostrarFarmacias to use async data fetching

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,4 @@
-import { ciclosData, farmaciasCoords, obtenerCicloActual, formatearFechaTurno, limpiarTelefono } from './data.js';
+import { ciclosData, farmaciasCoords, obtenerCicloActual, obtenerFarmaciasDelDia, formatearFechaTurno, limpiarTelefono } from './data.js';
 import { agregarMarcadores, limpiarMarcadores, mapDesktop, mapMobile, markersDesktop, markersMobile } from './maps.js';
 
 let activeCard = null;
@@ -25,9 +25,8 @@ export function getPhoneIcon() {
   return `<span class="icon-phone"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" fill="var(--accent)"/></svg></span>`;
 }
 
-export function mostrarFarmacias() {
-  const ciclo = obtenerCicloActual();
-  const farmacias = [...(ciclosData[ciclo] || [])].sort((a, b) => a.nombre.localeCompare(b.nombre));
+export async function mostrarFarmacias() {
+  const farmacias = await obtenerFarmaciasDelDia();
   const fecha = formatearFechaTurno();
   const intro = document.getElementById('intro');
   const lista = document.getElementById('lista');
